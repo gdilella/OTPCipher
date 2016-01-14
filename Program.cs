15c33d3706051a91a700891854703d39d7c4fd27
Gianmarco Di Lella <giammyjet@gmail.com>
2016-01-14 14:28:04 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTPCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OTPCipher by Gianmarco Di Lella CC-BY-SA");
+            Console.WriteLine("C: Cipher | D: Decipher");
+            Console.WriteLine("Choose mode:");
+            string mode = Console.ReadLine();
+            if(mode == "c" || mode == "C")
+            {
+                Console.WriteLine("Insert plaintext: ");
+                Vernam v = new Vernam();
+                v.Plaintext = Console.ReadLine();
+                v.GenerateKey();
+                Console.WriteLine("KEY: " + v.Key);
+                v.Cipher();
+                Console.WriteLine("CIPHERTEXT: " + v.Ciphertext);
+            }
+            else if(mode == "d" || mode == "D")
+            {
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vernam.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OTPCipher
+{
+    class Vernam
+    {
+        char[] plaintext;
+        char[] key;
+        char[] ciphertext;
+
+        public string Plaintext
+        {
+            get
+            {
+                return new String(plaintext);
+            }
+
+            set
+            {
+                Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+                plaintext = rgx.Replace(value, "").ToLower().Replace(" ","").ToCharArray();
+            }
+        }
+
+        public string Key
+        {
+            get
+            {
+                return new String(key);
+            }
+
+            set
+            {
+                key = value.ToCharArray();
+            }
+        }
+
+        public string Ciphertext
+        {
+            get
+            {
+                return new string(ciphertext);
+            }
+
+            set
+            {
+                ciphertext = value.ToCharArray();
+            }
+        }
+
+        public void Cipher() { }
+
+        public void Decipher() { }
+
+        public void GenerateKey()
+        {
+            Random r = new Random();
+            key = new char[plaintext.Length - 1];
+
+            for(int i = 0; i < plaintext.Length; i++)
+            {
+                
+            }
+        }
+    }
+}
